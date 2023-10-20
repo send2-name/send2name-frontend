76bc6ccd0feac8cdb1decd44b37105383d7e3b40
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2023-10-20 10:28:35 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -118,7 +118,7 @@ export default {
 		const userStore = useUserStore();
 
 		const coinbaseConnector = new CoinbaseWalletConnector({
-			appName: 'Vue Dapp',
+			appName: 'Send2.name',
 			jsonRpcUrl: rpcs["1"],
 		});
 
